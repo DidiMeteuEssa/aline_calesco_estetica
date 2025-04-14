6694682a6f0a5d20cc99e643e8cf7b13d9402791
Diego Calesco <diegolppe@gmail.com>
2025-04-14 06:15:19 -0300

    Bug background

---
+++ mascaras.js
@@ -0,0 +1,4 @@
+Inputmask("999.999.999-99").mask("#cpf");
+Inputmask("99/99/9999").mask("#data_nasc");
+Inputmask("99999-999").mask("#cep");
+Inputmask("(99) 99999-9999").mask("#celular");
---
+++ validacao_clientes.js
@@ -0,0 +1,35 @@
+function validarCampos() {
+    const cpf = document.getElementById("cpf").value.replace(/\D/g, '');
+    const cep = document.getElementById("cep").value.replace(/\D/g, '');
+    const celular = document.getElementById("celular").value.replace(/\D/g, '');
+    const data_nasc = document.getElementById("data_nasc").value.replace(/\D/g, '');
+
+    if (cpf.length !== 11) {
+        alert("CPF inválido! Digite os 11 números.");
+        document.getElementById("cpf").focus();
+        return false;
+    }
+
+    if (cep.length !== 8) {
+        alert("CEP inválido! Deve conter 8 números.");
+        document.getElementById("cep").focus();
+        return false;
+    }
+
+    if (celular.length !== 11) {
+        alert("Celular inválido! Deve conter 11 números (DDD + número).");
+        document.getElementById("celular").focus();
+        return false;
+    }
+
+    if (data_nasc.length !== 8) {
+        alert("Data de nascimento inválida! Preencha a data corretamente.");
+        document.getElementById("data_nasc").focus();
+        return false;
+    }
+
+    alert("Cadastro realizado com sucesso!");
+    return true;
+}
+
+
