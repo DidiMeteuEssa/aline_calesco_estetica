126938c3035932f4f6c44d11337ce6d54cd072c1
Diego Calesco <diegolppe@gmail.com>
2025-04-24 16:09:54 -0300

    bug

--- php_files/opr_clientes.php
+++ php_files/opr_clientes.php
@@ -75,64 +75,55 @@ function opr_cadastro()
 function opr_alterar()
 {
 
-    include("conexao_db.php");
+include("conexao_db.php");
 
 
+$cpf = $_POST["cpf"];
+
+    $nome = $_POST["nome"];
     $cpf = $_POST["cpf"];
+    $data_nasc = $_POST["data_nasc"];
+    $idade = $_POST["idade"];
+    $endereco = $_POST["endereco"];
+    $cep = $_POST["cep"];
+    $bairro = $_POST["bairro"];
+    $cidade = $_POST["cidade"];
+    $estado = $_POST["estado"];
+    $profissao = $_POST["profissao"];
+    $tel_res = $_POST["tel_res"];
+    $tel_com = $_POST["tel_com"];
+    $celular = $_POST["celular"];
+    $cpf_anterior = $_POST["cpf_anterior"];
 
-    $sql = "SELECT * FROM clientes WHERE cpf = '$cpf'";
-    $result = mysqli_query($conn, $sql);
+    $data_mysql = date('Y-m-d', strtotime(str_replace('/', '-', $data_nasc)));
 
-    if (mysqli_num_rows($result) > 0) {
-        echo "<script>alert('Este CPF já existe!'); window.history.back();</script>";
+    $sql = "UPDATE clientes SET cpf = ?, nome = ?, data_nasc = ?, idade = ?, endereco = ?, cep = ?, bairro = ?,
+                            cidade  = ?, estado  = ?, profissao = ?, tel_res  = ?, tel_com  = ?, tel_cel  = ?
+                            WHERE cpf = ?";
+    $stmt = $conn->prepare($sql);
+
+    if ($stmt) {
+        $stmt->bind_param(
+            "sssissssssssss",
+            $cpf,
+            $nome,
+            $data_mysql,
+            $idade,
+            $endereco,
+            $cep,
+            $bairro,
+            $cidade,
+            $estado,
+            $profissao,
+            $tel_res,
+            $tel_com,
+            $celular,
+            $cpf_anterior
+        );
+        $stmt->execute();
+        echo "<script>alert('Alteração concluída!'); window.location.href = 'alterar_clientes.php';</script>";
         exit;
     } else {
-
-        $nome = $_POST["nome"];
-        $cpf = $_POST["cpf"];
-        $data_nasc = $_POST["data_nasc"];
-        $idade = $_POST["idade"];
-        $endereco = $_POST["endereco"];
-        $cep = $_POST["cep"];
-        $bairro = $_POST["bairro"];
-        $cidade = $_POST["cidade"];
-        $estado = $_POST["estado"];
-        $profissao = $_POST["profissao"];
-        $tel_res = $_POST["tel_res"];
-        $tel_com = $_POST["tel_com"];
-        $celular = $_POST["celular"];
-        $cpf_anterior = $_POST["cpf_anterior"];
-
-        $data_mysql = date('Y-m-d', strtotime(str_replace('/', '-', $data_nasc)));
-
-        $sql = "UPDATE clientes SET cpf = ?, nome = ?, data_nasc = ?, idade = ?, endereco = ?, cep = ?, bairro = ?,
-                                cidade  = ?, estado  = ?, profissao = ?, tel_res  = ?, tel_com  = ?, tel_cel  = ?
-                                WHERE cpf = ?";
-        $stmt = $conn->prepare($sql);
-
-        if ($stmt) {
-            $stmt->bind_param(
-                "sssissssssssss",
-                $cpf,
-                $nome,
-                $data_mysql,
-                $idade,
-                $endereco,
-                $cep,
-                $bairro,
-                $cidade,
-                $estado,
-                $profissao,
-                $tel_res,
-                $tel_com,
-                $celular,
-                $cpf_anterior
-            );
-            $stmt->execute();
-            echo "<script>alert('Alteração concluída!'); window.location.href = 'alterar_clientes.php';</script>";
-            exit;
-        } else {
-            echo "Erro ao alterar cliente.";
-        }
+        echo "Erro ao alterar cliente.";
     }
 }
