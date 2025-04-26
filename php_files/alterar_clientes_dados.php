cf31955aa600c0281e31cfc7fddbdcc90dcd5581
Diego Calesco <diegolppe@gmail.com>
2025-04-26 16:36:31 -0300

    cadastro e alteração melhorados + box melhorado

--- php_files/alterar_clientes_dados.php
+++ php_files/alterar_clientes_dados.php
@@ -27,47 +27,76 @@ $data_br = date('d/m/Y', strtotime($data_mysql));
             <form method="post" action="opr_clientes.php" class="data-form" onsubmit="return validarCampos(2)">
                 <input type="hidden" name="opr_cliente" value="2">
                 <input type="hidden" name="cpf_anterior" value="<?= $_POST["cpf_lista"]; ?>">
-                <table class="data-table">
-                    <tbody>
-                        <tr>
-                            <td><label for="nome">Nome</label></td>
-                            <td><input type="text" id="nome" name="nome" placeholder="Digite o nome" value="<?= $_POST["nome_lista"]; ?>" required></td>
-                            <td><label for="endereco">Endereço</label></td>
-                            <td><input type="text" id="endereco" name="endereco" placeholder="Digite o endereço" value="<?= $_POST["endereco_lista"]; ?>" required></td>
-                            <td><label for="tel_residencial">Telefone Residencial</label></td>
-                            <td><input type="text" id="tel_res" name="tel_res" placeholder="Digite o telefone residencial" value="<?= $_POST["tel_res_lista"]; ?>"></td>
-                        </tr>
-                        <tr>
-                            <td><label for="cpf">CPF</label></td>
-                            <td><input type="text" id="cpf" name="cpf" placeholder="000.000.000-00" value="<?= $_POST["cpf_lista"]; ?>" required></td>
-                            <td><label for="cep">CEP</label></td>
-                            <td><input type="text" id="cep" name="cep" placeholder="00000-000" value="<?= $_POST["cep_lista"]; ?>" required></td>
-                            <td><label for="tel_comercial">Telefone Comercial</label></td>
-                            <td><input type="text" id="tel_com" name="tel_com" placeholder="Digite o telefone comercial" value="<?= $_POST["tel_com_lista"]; ?>"></td>
-                        </tr>
-                        <tr>
-                            <td><label for="data_nasc">Data de Nascimeto</label></td>
-                            <td><input type="text" id="data_nasc" name="data_nasc" placeholder="dd/mm/aaaa" value="<?= $data_br  ?>" required></td>
-                            <td><label for="cidade">Cidade</label></td>
-                            <td><input type="text" id="cidade" name="cidade" placeholder="Digite a cidade" value="<?= $_POST["cidade_lista"]; ?>" required></td>
-                            <td><label for="celular">Telefone Celular</label></td>
-                            <td><input type="text" id="celular" name="celular" placeholder="(00) 00000 - 0000" value="<?= $_POST["celular_lista"]; ?>" required></td>
-                        </tr>
-                        <tr>
-                            <td><label for="idade">Idade</label></td>
-                            <td><input type="text" id="idade" name="idade" placeholder="Digite a idade" value="<?= $_POST["idade_lista"]; ?>" required></td>
-                            <td><label for="bairro">Bairro</label></td>
-                            <td><input type="text" id="bairro" name="bairro" placeholder="Digite o bairro" value="<?= $_POST["bairro_lista"]; ?>" required></td>
-                        </tr>
-                        <tr>
-                            <td><label for="profissao">Profissão</label></td>
-                            <td><input type="text" id="profissao" value="<?= $_POST["profissao_lista"]; ?>" name="profissao" placeholder="Digite a profissão" maxlength="30" required></td>
-                            <td><label for="estado">Estado</label></td>
-                            <td><input type="text" id="estado" name="estado" placeholder="SP / RJ / MG /..." maxlength="2" value="<?= $_POST["estado_lista"]; ?>" required></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <button type="submit">Enviar</button>
+                <section class="data-cliente">
+                    <form method="post" action="opr_clientes.php" class="data-form" onsubmit="return validarCampos(1)">
+                        <input type="hidden" name="opr_cliente" value="1">
+                        <ul class="data-ul">
+                            <li>
+                                <label for="nome">Nome</label>
+                                <input type="text" id="nome" name="nome" placeholder="Digite o nome" value="<?= $_POST["nome_lista"]; ?>" required>
+                            </li>
+                            <li>
+                                <label for="endereco">Endereço</label>
+                                <input type="text" id="endereco" name="endereco" placeholder="Digite o endereço" value="<?= $_POST["endereco_lista"]; ?>" required>
+                            </li>
+                            <li>
+                                <label for="tel_residencial">Telefone Residencial</label>
+                                <input type="text" id="tel_res" name="tel_res" value="<?= $_POST["tel_res_lista"]; ?>" placeholder="Digite o telefone residencial">
+                            </li>
+                        </ul>
+                        <ul class="data-ul">
+                            <li>
+                                <label for="cpf">CPF</label>
+                                <input type="text" id="cpf" name="cpf" value="<?= $_POST["cpf_lista"]; ?>" placeholder="000.000.000-00" required>
+                            </li>
+                            <li>
+                                <label for="cep">CEP</label>
+                                <input type="text" id="cep" name="cep" value="<?= $_POST["cep_lista"]; ?>" placeholder="00000-000" required>
+                            </li>
+                            <li>
+                                <label for="tel_comercial">Telefone Comercial</label>
+                                <input type="text" id="tel_com" name="tel_com" value="<?= $_POST["tel_com_lista"]; ?>" placeholder="Digite o telefone comercial">
+                            </li>
+                        </ul>
+                        <ul class="data-ul">
+                            <li>
+                                <label for="data_nasc">Data de Nascimeto</label>
+                                <input type="text" id="data_nasc" name="data_nasc" value="<?= $data_br  ?>" placeholder="dd/mm/aaaa" required>
+                            </li>
+                            <li>
+                                <label for="cidade">Cidade</label>
+                                <input type="text" id="cidade" name="cidade" value="<?= $_POST["cidade_lista"]; ?>" placeholder="Digite a cidade" required>
+                            </li>
+                            <li>
+                                <label for="celular">Telefone Celular</label>
+                                <input type="text" id="celular" name="celular" value="<?= $_POST["celular_lista"]; ?>" placeholder="(00) 00000 - 0000" required>
+                            </li>
+                        </ul>
+                        <ul class="data-ul">
+                            <li>
+                                <label for="idade">Idade</label>
+                                <input type="text" id="idade" name="idade" value="<?= $_POST["idade_lista"]; ?>" placeholder="Digite a idade" required>
+                            </li>
+                            <li>
+                                <label for="bairro">Bairro</label>
+                                <input type="text" id="bairro" name="bairro" value="<?= $_POST["bairro_lista"]; ?>" placeholder="Digite o bairro" required>
+                            </li>
+                            <li></li>
+                        </ul>
+                        <ul class="data-ul">
+                            <li>
+                                <label for="profissao">Profissão</label>
+                                <input type="text" id="profissao" name="profissao" placeholder="Digite a profissão" value="<?= $_POST["profissao_lista"]; ?>" maxlength="200" required>
+                            </li>
+                            <li>
+                                <label for="estado">Estado</label>
+                                <input type="text" id="estado" name="estado" placeholder="SP / RJ / MG /..." value="<?= $_POST["estado_lista"]; ?>" maxlength="2" required>
+                            </li>
+                            <li></li>
+                        </ul>
+                </section>
+                <section>
+                    <button class="enviar" type="submit">Enviar</button>
             </form>
         </section>
     </main>
--- php_files/cadastro_clientes.php
+++ php_files/cadastro_clientes.php
@@ -18,50 +18,76 @@
     </header>
     <main>
         <h2>Cadastro de Clientes / <a href="../index.php">VOLTAR</a></h2>
-        <section class="data">
+        <section class="data-cliente">
             <form method="post" action="opr_clientes.php" class="data-form" onsubmit="return validarCampos(1)">
                 <input type="hidden" name="opr_cliente" value="1">
-                <table class="data-table">
-                    <tbody>
-                        <tr>
-                            <td><label for="nome">Nome</label></td>
-                            <td><input type="text" id="nome" name="nome" placeholder="Digite o nome" required></td>
-                            <td><label for="endereco">Endereço</label></td>
-                            <td><input type="text" id="endereco" name="endereco" placeholder="Digite o endereço" required></td>
-                            <td><label for="tel_residencial">Telefone Residencial</label></td>
-                            <td><input type="text" id="tel_res" name="tel_res" placeholder="Digite o telefone residencial"></td>
-                        </tr>
-                        <tr>
-                            <td><label for="cpf">CPF</label></td>
-                            <td><input type="text" id="cpf" name="cpf" placeholder="000.000.000-00" required></td>
-                            <td><label for="cep">CEP</label></td>
-                            <td><input type="text" id="cep" name="cep" placeholder="00000-000" required></td>
-                            <td><label for="tel_comercial">Telefone Comercial</label></td>
-                            <td><input type="text" id="tel_com" name="tel_com" placeholder="Digite o telefone comercial"></td>
-                        </tr>
-                        <tr>
-                            <td><label for="data_nasc">Data de Nascimeto</label></td>
-                            <td><input type="text" id="data_nasc" name="data_nasc" placeholder="dd/mm/aaaa" required></td>
-                            <td><label for="cidade">Cidade</label></td>
-                            <td><input type="text" id="cidade" name="cidade" placeholder="Digite a cidade" required></td>
-                            <td><label for="celular">Telefone Celular</label></td>
-                            <td><input type="text" id="celular" name="celular" placeholder="(00) 00000 - 0000" required></td>
-                        </tr>
-                        <tr>
-                            <td><label for="idade">Idade</label></td>
-                            <td><input type="text" id="idade" name="idade" placeholder="Digite a idade" required></td>
-                            <td><label for="bairro">Bairro</label></td>
-                            <td><input type="text" id="bairro" name="bairro" placeholder="Digite o bairro" required></td>
-                        </tr>
-                        <tr>
-                            <td><label for="profissao">Profissão</label></td>
-                            <td><input type="text" id="profissao" name="profissao" placeholder="Digite a profissão" maxlength="200" required></td>
-                            <td><label for="estado">Estado</label></td>
-                            <td><input type="text" id="estado" name="estado" placeholder="SP / RJ / MG /..." maxlength="2" required></td>
-                        </tr>
-                    </tbody>
-                </table>
-                <button type="submit">Enviar</button>
+                <ul class="data-ul">
+                    <li>
+                        <label for="nome">Nome</label>
+                        <input type="text" id="nome" name="nome" placeholder="Digite o nome" required>
+                    </li>
+                    <li>
+                        <label for="endereco">Endereço</label>
+                        <input type="text" id="endereco" name="endereco" placeholder="Digite o endereço" required>
+                    </li>
+                    <li>
+                        <label for="tel_residencial">Telefone Residencial</label>
+                        <input type="text" id="tel_res" name="tel_res" placeholder="Digite o telefone residencial">
+                    </li>
+                </ul>
+                <ul class="data-ul">
+                    <li>
+                        <label for="cpf">CPF</label>
+                        <input type="text" id="cpf" name="cpf" placeholder="000.000.000-00" required>
+                    </li>
+                    <li>
+                        <label for="cep">CEP</label>
+                        <input type="text" id="cep" name="cep" placeholder="00000-000" required>
+                    </li>
+                    <li>
+                        <label for="tel_comercial">Telefone Comercial</label>
+                        <input type="text" id="tel_com" name="tel_com" placeholder="Digite o telefone comercial">
+                    </li>
+                </ul>
+                <ul class="data-ul">
+                    <li>
+                        <label for="data_nasc">Data de Nascimeto</label>
+                        <input type="text" id="data_nasc" name="data_nasc" placeholder="dd/mm/aaaa" required>
+                    </li>
+                    <li>
+                        <label for="cidade">Cidade</label>
+                        <input type="text" id="cidade" name="cidade" placeholder="Digite a cidade" required>
+                    </li>
+                    <li>
+                        <label for="celular">Telefone Celular</label>
+                        <input type="text" id="celular" name="celular" placeholder="(00) 00000 - 0000" required>
+                    </li>
+                </ul>
+                <ul class="data-ul">
+                    <li>
+                        <label for="idade">Idade</label>
+                        <input type="text" id="idade" name="idade" placeholder="Digite a idade" required>
+                    </li>
+                    <li>
+                        <label for="bairro">Bairro</label>
+                        <input type="text" id="bairro" name="bairro" placeholder="Digite o bairro" required>
+                    </li>
+                    <li></li>
+                </ul>
+                <ul class="data-ul">
+                    <li>
+                        <label for="profissao">Profissão</label>
+                        <input type="text" id="profissao" name="profissao" placeholder="Digite a profissão" maxlength="200" required>
+                    </li>
+                    <li>
+                        <label for="estado">Estado</label>
+                        <input type="text" id="estado" name="estado" placeholder="SP / RJ / MG /..." maxlength="2" required>
+                    </li>
+                    <li></li>
+                </ul>
+        </section>
+        <section>
+            <button class="enviar" type="submit">Enviar</button>
             </form>
         </section>
     </main>
